f1194e79a124ce65d2332f2d982de52509063552
orseL <freundliche.alien@gmail.com>
2018-04-01 18:56:17 +0900

    Config
    2018.04.01 Add Config for Engine

---
+++ Config/Config.cpp
@@ -0,0 +1,101 @@
+#include "Config.h"
+#define CONFIG_BUFFER_MAX 64
+
+// public
+Config::Config()
+{
+	mConfigPath = L"";
+}
+
+Config::~Config()
+{
+	mConfigPath.clear();
+}
+
+void Config::Initialize(wstring configPath)
+{
+	mConfigPath = configPath;
+
+	for (int type = 0; type < (int)CONFIG_SECTION_TYPE_COUNT; type++)
+	{
+		SetConfig((CS_Type)type);
+	}
+}
+
+// private
+void Config::SetConfig(CS_Type type)
+{
+	switch (type)
+	{
+	case CS_Engine:
+	{
+		ZeroMemory(&gCFEngine, sizeof(CF_ENGINE));
+
+		gCFEngine.Name			= GetValueString(ENGINE, L"Name");
+		gCFEngine.Windowed		= GetValueBOOL(ENGINE, L"Windowed");
+		gCFEngine.VsyncEnabled	= GetValueBOOL(ENGINE, L"VsyncEnabled");
+		
+		UINT screenX			= GetValueUINT(ENGINE, L"ScreenX");
+		UINT screenY			= GetValueUINT(ENGINE, L"ScreenY");
+		FLOAT screenNear		= GetValueFLOAT(ENGINE, L"ScreenNear");
+		FLOAT screenDepth		= GetValueFLOAT(ENGINE, L"ScreenDepth");
+		FLOAT cameraSpeed		= GetValueFLOAT(ENGINE, L"CameraSpeed");
+
+		gCFEngine.ScreenX		= Clamp(screenX, (UINT)500, (UINT)GetSystemMetrics(SM_CXSCREEN));
+		gCFEngine.ScreenY		= Clamp(screenY, (UINT)500, (UINT)GetSystemMetrics(SM_CYSCREEN));
+		gCFEngine.ScreenNear	= Clamp(screenNear, 0.1F, 1.0F);
+		gCFEngine.ScreenDepth	= Clamp(screenDepth, 100.0F, 1000.0F);
+		gCFEngine.CameraSpeed	= Clamp(cameraSpeed, 10.0F, 100.0F);
+
+		break;
+	}
+
+	case CS_Log:
+	{break;}
+
+	case CS_Terrain:
+	{break;}
+
+	}
+}
+
+const wstring Config::GetValueString(const WCHAR* section, const WCHAR* key)
+{
+	WCHAR output[CONFIG_BUFFER_MAX];
+
+	GetPrivateProfileString(section, key, L"", output, ARRAYSIZE(output), mConfigPath.c_str());
+
+	return wstring(output);
+}
+
+const UINT Config::GetValueUINT(const WCHAR* section, const WCHAR* key)
+{
+	UINT output = GetPrivateProfileInt(section, key, (UINT)0, mConfigPath.c_str());
+
+	// **음수가 들어오면 UINT_MAX값이 들어옴
+	if (output >= UINT_MAX)
+		return 0;
+
+	return output;
+}
+
+const FLOAT Config::GetValueFLOAT(const WCHAR* section, const WCHAR* key)
+{
+	WCHAR output[CONFIG_BUFFER_MAX];
+
+	GetPrivateProfileString(section, key, L"0.0", output, ARRAYSIZE(output), mConfigPath.c_str());
+
+	return (FLOAT)_wtof(output);
+}
+
+const BOOL Config::GetValueBOOL(const WCHAR* section, const WCHAR* key)
+{
+	WCHAR output[CONFIG_BUFFER_MAX];
+
+	GetPrivateProfileString(section, key, L"FALSE", output, ARRAYSIZE(output), mConfigPath.c_str());
+
+	if ((wstring(output).find(L"true")) == 0 || (wstring(output).find(L"TRUE")) == 0)
+		return TRUE;
+
+	return FALSE;
+}
---
+++ Config/Config.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "..\Common\Pch.h"
+
+class Config
+{
+public:
+	Config();
+	~Config();
+	void Initialize(wstring configPath);
+
+private:
+	typedef enum CONFIG_SECTION_TYPE_ENUM
+	{
+		CS_Engine = 0,
+		CS_Log,
+		CS_Terrain,
+		CS_SkySphere,
+		CS_SkyPlane,
+		CS_Water,
+		CONFIG_SECTION_TYPE_COUNT,
+	} CS_Type;
+
+	// Section °ª
+	const WCHAR ENGINE[7]		= L"Engine";
+	const WCHAR LOG[4]			= L"Log";
+	const WCHAR TERRAIN[8]		= L"Terrain";
+	const WCHAR SKYSPHERE[10]	= L"SkySphere";
+	const WCHAR SKYPLANE[9]		= L"SkyPlane";
+	const WCHAR WATER[6]		= L"Water";
+
+	wstring mConfigPath;
+
+	void SetConfig(CS_Type type);
+	const wstring GetValueString(const WCHAR* section, const WCHAR* key);
+	const UINT GetValueUINT(const WCHAR* section, const WCHAR* key);
+	const FLOAT GetValueFLOAT(const WCHAR* section, const WCHAR* key);
+	const BOOL GetValueBOOL(const WCHAR* section, const WCHAR* key);
+};
+
