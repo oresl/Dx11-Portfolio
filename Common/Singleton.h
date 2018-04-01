699e34d184524a5688bb2a5908675422de281a74
orseL <freundliche.alien@gmail.com>
2018-04-01 18:57:14 +0900

    Singleton
    2018.04.01 Direct3D

---
+++ Common/Singleton.h
@@ -0,0 +1,26 @@
+#pragma once
+// h에 입력
+#define Singleton_h(classname)\
+private:\
+	static classname* sInstance;\
+public:\
+	static classname& GetInstance();\
+	static void Delete();
+
+// cpp에 입력
+#define Singleton_cpp(classname)\
+classname* classname::sInstance = NULL;\
+classname& classname::GetInstance()\
+{\
+	if (sInstance == NULL)\
+		sInstance = new classname;\
+	return *sInstance;\
+}\
+void classname::Delete()\
+{\
+	SafeDeletePtr(sInstance);\
+}
+
+#define GetInst(classname) classname::GetInstance()
+
+#define D3D GetInst(Direct3D)
