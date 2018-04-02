5500422e208f727ee96dd44e67cb286f9b9c1f8e
orseL <freundliche.alien@gmail.com>
2018-04-03 05:37:47 +0900

    Functions

--- Common/Func.h
+++ Common/Func.h
@@ -37,3 +37,58 @@ namespace func_dir
 		return false;
 	}
 }
+
+namespace func_shader_buffer
+{
+	static void CreateBuffer(ID3D11Device* device, D3D11_BIND_FLAG flag, UINT byteWidth, D3D11_USAGE usage,
+		void* data, ID3D11Buffer** buffer)
+	{
+		D3D11_BUFFER_DESC bufferDesc;
+		ZeroMemory(&bufferDesc, sizeof(D3D11_BUFFER_DESC));
+		bufferDesc.BindFlags		= flag;
+		bufferDesc.ByteWidth		= byteWidth;
+		bufferDesc.Usage			= usage;
+
+		if (usage == D3D11_USAGE_DYNAMIC)
+			bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+		else
+			bufferDesc.CPUAccessFlags = 0;
+
+		if (data != NULL)
+		{
+			D3D11_SUBRESOURCE_DATA bufferData;
+			ZeroMemory(&bufferData, sizeof(D3D11_SUBRESOURCE_DATA));
+			bufferData.pSysMem = data;
+
+			HRESULT hr = device->CreateBuffer(&bufferDesc, &bufferData, buffer);
+			assert(SUCCEEDED(hr));
+		}
+		else
+		{
+			HRESULT hr = device->CreateBuffer(&bufferDesc, NULL, buffer);
+			assert(SUCCEEDED(hr));
+		}
+	}
+
+	static void SetConstantBuffer(ID3D11DeviceContext* context, ID3D11Buffer* buffer, const void* data, UINT dataSize, UINT startSlot)
+	{
+		D3D11_MAPPED_SUBRESOURCE mapped;
+		HRESULT hr = context->Map(buffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mapped);
+		assert(SUCCEEDED(hr));
+
+		memcpy(mapped.pData, data, (size_t)dataSize);
+
+		// 공용버퍼 (Pcb.fx)
+		// 0 - World
+		// 1 - Camera
+		// 2 - Light
+
+		context->Unmap(buffer, 0);
+		context->VSSetConstantBuffers(startSlot, 1, &buffer);
+		context->HSSetConstantBuffers(startSlot, 1, &buffer);
+		context->DSSetConstantBuffers(startSlot, 1, &buffer);
+		context->PSSetConstantBuffers(startSlot, 1, &buffer);
+	}
+
+	// Texture, SamplerState
+}
