6eea52b19d0cab784e157a5f81dff63dde165865
orseL <freundliche.alien@gmail.com>
2018-04-03 05:38:49 +0900

    Customized Input Elements

---
+++ RenderResource/InputLayout.cpp
@@ -0,0 +1,130 @@
+#include "InputLayout.h"
+#define FLOAT1			DXGI_FORMAT_R32_FLOAT
+#define FLOAT2			DXGI_FORMAT_R32G32_FLOAT
+#define FLOAT3			DXGI_FORMAT_R32G32B32_FLOAT
+#define FLOAT4			DXGI_FORMAT_R32G32B32A32_FLOAT
+#define VERTEX_DATA		D3D11_INPUT_PER_VERTEX_DATA
+#define INSTANCE_DATA	D3D11_INPUT_PER_INSTANCE_DATA
+#define ALIGNED			D3D11_APPEND_ALIGNED_ELEMENT
+
+#pragma region D3D11_INPUT_ELEMENT_DESC 커스터마이징 구조체 정의
+using namespace inputlayout;
+
+const D3D11_INPUT_ELEMENT_DESC IL_Pos::sElementDesc[] =
+{
+	{ "POSITION",	0, FLOAT3, 0,		0, VERTEX_DATA, 0 },
+};
+const UINT IL_Pos::sElementCount = ARRAYSIZE(sElementDesc);
+
+const D3D11_INPUT_ELEMENT_DESC IL_PosTex::sElementDesc[] =
+{
+	{ "POSITION",	0, FLOAT3, 0,		0, VERTEX_DATA, 0 },
+	{ "TEXCOORD",	0, FLOAT2, 0, ALIGNED, VERTEX_DATA, 0 },
+};
+const UINT IL_PosTex::sElementCount = ARRAYSIZE(sElementDesc);
+
+const D3D11_INPUT_ELEMENT_DESC IL_PosTexNor::sElementDesc[] =
+{
+	{ "POSITION",	0, FLOAT3, 0,		0, VERTEX_DATA, 0 },
+	{ "TEXCOORD",	0, FLOAT2, 0, ALIGNED, VERTEX_DATA, 0 },
+	{ "NORMAL"	,	0, FLOAT3, 0, ALIGNED, VERTEX_DATA, 0 },
+};
+const UINT IL_PosTexNor::sElementCount = ARRAYSIZE(sElementDesc);
+
+const D3D11_INPUT_ELEMENT_DESC IL_PosTexNorTanBiN::sElementDesc[] =
+{
+	{ "POSITION",	0, FLOAT3, 0,		0, VERTEX_DATA, 0 },
+	{ "TEXCOORD",	0, FLOAT2, 0, ALIGNED, VERTEX_DATA, 0 },
+	{ "NORMAL"	,	0, FLOAT3, 0, ALIGNED, VERTEX_DATA, 0 },
+	{ "TANGENT"	,	0, FLOAT3, 0, ALIGNED, VERTEX_DATA, 0 },
+	{ "BINORMAL",	0, FLOAT3, 0, ALIGNED, VERTEX_DATA, 0 },
+};
+const UINT IL_PosTexNorTanBiN::sElementCount = ARRAYSIZE(sElementDesc);
+
+const D3D11_INPUT_ELEMENT_DESC IL_Instance::sElementDesc[] =
+{
+	{ "POSITION",	0, FLOAT3, 0,		0, VERTEX_DATA	, 0 },
+	{ "WORLD",		0, FLOAT4, 1,		0, INSTANCE_DATA, 1 },
+	{ "WORLD",		1, FLOAT4, 1, ALIGNED, INSTANCE_DATA, 1 },
+	{ "WORLD",		2, FLOAT4, 1, ALIGNED, INSTANCE_DATA, 1 },
+	{ "WORLD",		3, FLOAT4, 1, ALIGNED, INSTANCE_DATA, 1 },
+};
+const UINT IL_Instance::sElementCount = ARRAYSIZE(sElementDesc);
+
+const D3D11_INPUT_ELEMENT_DESC IL_Extra_0::sElementDesc[] =
+{
+	{ "POSITION",	0, FLOAT3, 0,		0, VERTEX_DATA, 0 },
+	{ "TEXCOORD",	0, FLOAT3, 0, ALIGNED, VERTEX_DATA, 0 },
+};
+const UINT IL_Extra_0::sElementCount = ARRAYSIZE(sElementDesc);
+#pragma endregion
+
+// public
+bool InputLayout::CheckInstanced()				{ return mInstanced; }
+bool InputLayout::CheckTessellated()			{ return mTessellated; }
+ID3D11Buffer* InputLayout::GetVertexBuffer()	{ return mVertexBuffer; }
+ID3D11Buffer* InputLayout::GetIndexBuffer()		{ return mIndexBuffer; }
+ID3D11Buffer* InputLayout::GetInstanceBuffer()	{ return mInstanceBuffer; }
+UINT InputLayout::GetVertexCount()				{ return mVertexCount; }
+UINT InputLayout::GetIndexCount()				{ return mIndexCount; }
+UINT InputLayout::GetInstanceCount()			{ return mInstanceCount; }
+UINT InputLayout::GetDrawCount()				{ return mDrawCount; }
+UINT InputLayout::GetInputLayoutSize()			{ return mInputLayoutSize; }
+UINT InputLayout::GetInputLayoutInstanceSize()	{ return mInputLayoutInstanceSize; }
+
+// protected
+InputLayout::InputLayout()
+{
+	D3DXMatrixIdentity(&mWorld);
+	mInstanced					= false;
+	mTessellated				= false;
+	mVertexBuffer				= NULL;
+	mIndexBuffer				= NULL;
+	mInstanceBuffer				= NULL;
+	mVertexCount				= 0;
+	mIndexCount					= 0;
+	mInstanceCount				= 0;
+	mDrawCount					= 0;
+	mInputLayoutSize			= 0;
+	mInputLayoutInstanceSize	= 0;
+}
+
+InputLayout::~InputLayout()
+{
+	SafeReleaseCom(mInstanceBuffer);
+	SafeReleaseCom(mIndexBuffer);
+	SafeReleaseCom(mVertexBuffer);
+}
+
+void InputLayout::GetInputLayoutFromFile(wstring meshName)
+{
+	wstring path = L"Data\\Mesh\\" + meshName + L".txt";
+
+	wifstream file(path.c_str());
+	assert(!file.fail());
+
+	WCHAR output;
+	file.get(output);
+	while (output != '=')
+	{
+		file.get(output);
+	}
+
+	file >> mVertexCount;
+	mIndexCount = mVertexCount;
+	mTempData.resize(mVertexCount);
+
+	file.get(output);
+	for (UINT i = 0; i < mVertexCount; i++)
+	{
+		D3DXVECTOR3 pos;
+		D3DXVECTOR3 tex;
+		D3DXVECTOR3 nor;
+
+		file >> mTempData[i].Pos.x >> mTempData[i].Pos.y >> mTempData[i].Pos.z;
+		file >> mTempData[i].Tex.x >> mTempData[i].Tex.y;
+		file >> mTempData[i].Nor.x >> mTempData[i].Nor.y >> mTempData[i].Nor.z;
+	}
+
+	file.close();
+}
---
+++ RenderResource/InputLayout.h
@@ -0,0 +1,115 @@
+#pragma once
+#include "..\Common\Pch.h"
+#include "..\Graphic\Direct3D.h"
+
+#pragma region D3D11_INPUT_ELEMENT_DESC 커스터마이징 구조체 정의
+namespace inputlayout
+{
+	typedef struct INPUT_LAYOUT_TYPE_0
+	{
+		D3DXVECTOR3 Pos;
+
+		static const D3D11_INPUT_ELEMENT_DESC sElementDesc[];
+		static const UINT sElementCount;
+
+	} IL_Pos;
+
+	typedef struct INPUT_LAYOUT_TYPE_1
+	{
+		D3DXVECTOR3 Pos;
+		D3DXVECTOR2 Tex;
+
+		static const D3D11_INPUT_ELEMENT_DESC sElementDesc[];
+		static const UINT sElementCount;
+
+	} IL_PosTex;
+
+	typedef struct INPUT_LAYOUT_TYPE_2
+	{
+		D3DXVECTOR3 Pos;
+		D3DXVECTOR2 Tex;
+		D3DXVECTOR3 Nor;
+
+		static const D3D11_INPUT_ELEMENT_DESC sElementDesc[];
+		static const UINT sElementCount;
+
+	} IL_PosTexNor;
+
+	typedef struct INPUT_LAYOUT_TYPE_3
+	{
+		D3DXVECTOR3 Pos;
+		D3DXVECTOR2 Tex;
+		D3DXVECTOR3 Nor;
+		D3DXVECTOR3 Tan;
+		D3DXVECTOR3 BiN;
+
+		static const D3D11_INPUT_ELEMENT_DESC sElementDesc[];
+		static const UINT sElementCount;
+
+	} IL_PosTexNorTanBiN;
+
+	typedef struct INPUT_LAYOUT_TYPE_4
+	{
+		D3DXMATRIX World;
+
+		static const D3D11_INPUT_ELEMENT_DESC sElementDesc[];
+		static const UINT sElementCount;
+
+	} IL_Instance;
+
+	typedef struct INPUT_LAYOUT_TYPE_EXTRA_0
+	{
+		D3DXVECTOR3 Pos;
+		D3DXVECTOR3 Tex;
+
+		static const D3D11_INPUT_ELEMENT_DESC sElementDesc[];
+		static const UINT sElementCount;
+
+	} IL_Extra_0;	 // Trapezoid
+}
+
+using namespace inputlayout;
+#pragma endregion
+
+class InputLayout
+{
+public:
+	bool					CheckInstanced();
+	bool					CheckTessellated();
+	ID3D11Buffer*			GetVertexBuffer();
+	ID3D11Buffer*			GetIndexBuffer();
+	ID3D11Buffer*			GetInstanceBuffer();
+	UINT					GetVertexCount();
+	UINT					GetIndexCount();
+	UINT					GetInstanceCount();
+	UINT					GetDrawCount();
+	UINT					GetInputLayoutSize();
+	UINT					GetInputLayoutInstanceSize();
+
+protected:
+	struct BufferData
+	{
+		D3DXMATRIX World;
+	};
+
+	D3DXMATRIX				mWorld;						// 인스턴싱은 사용x
+	ID3D11Buffer*			mWorldBuffer;
+	bool					mInstanced;
+	bool					mTessellated;
+	ID3D11Buffer*			mVertexBuffer;
+	ID3D11Buffer*			mIndexBuffer;
+	ID3D11Buffer*			mInstanceBuffer;
+	UINT					mVertexCount;
+	UINT					mIndexCount;
+	UINT					mInstanceCount;
+	UINT					mDrawCount;
+	UINT					mInputLayoutSize;			// Strides
+	UINT					mInputLayoutInstanceSize;
+	vector<IL_PosTexNor>	mTempData;
+
+	InputLayout();
+	virtual ~InputLayout();
+	void					GetInputLayoutFromFile(wstring meshName);
+	virtual void			InitializeData() = 0;
+	virtual void			SetBuffer() = 0;
+};
