65ac719b3fb77b3de3ba29ddb6fb55d7683d762b
orseL <freundliche.alien@gmail.com>
2018-04-03 05:39:15 +0900

    Shader

--- RenderResource/Shader.cpp
+++ RenderResource/Shader.cpp
@@ -3,11 +3,12 @@
 // public
 Shader::Shader()
 {
-	mShaderName		= L"";
-	mVertexShader	= NULL;
-	mHullShader		= NULL;
-	mDomainShader	= NULL;
-	mPixelShader	= NULL;
+	mName			= L"";
+	mTessellated	= false;
+	mVS				= NULL;
+	mHS				= NULL;
+	mDS				= NULL;
+	mPS				= NULL;
 	mInputLayout	= NULL;
 	mShaderBlob		= NULL;
 }
@@ -16,38 +17,53 @@ Shader::~Shader()
 {
 	SafeReleaseCom(mShaderBlob);
 	SafeReleaseCom(mInputLayout);
-	SafeReleaseCom(mPixelShader);
-	SafeReleaseCom(mDomainShader);
-	SafeReleaseCom(mHullShader);
-	SafeReleaseCom(mVertexShader);
-	mShaderName.clear();
+	SafeReleaseCom(mPS);
+	SafeReleaseCom(mDS);
+	SafeReleaseCom(mHS);
+	SafeReleaseCom(mVS);
+	mName.clear();
 }
 
 void Shader::Initialize(wstring shaderName, const D3D11_INPUT_ELEMENT_DESC* elementDesc, UINT elementCount, bool tessellated)
 {
-	mShaderName = shaderName;
+	mName			= shaderName;
+	mTessellated	= tessellated;
 
-	if (tessellated)
+	if (mTessellated)
 	{
 		InitializeShader(IS_Hull);
 		InitializeShader(IS_Domain);
 	}
 
 	// ** InputLayout 생성시 Vertex Blob이 필요하기 때문에, Vertex Shader버퍼를 가장 마지막에 생성
-	InitializeShader(IS_Vertex);
 	InitializeShader(IS_Pixel);
+	InitializeShader(IS_Vertex);
 
 	InitializeInputLayout(elementDesc, elementCount);
+}
+
+void Shader::SetShader()
+{
+	D3D.GetContext()->IASetInputLayout(mInputLayout);
+	D3D.GetContext()->VSSetShader(mVS, NULL, 0);
+	D3D.GetContext()->PSSetShader(mPS, NULL, 0);
+
+	//if (mTessellated)
+	//{
+	//	D3D.GetContext()->HSSetShader(mHS, NULL, 0);
+	//	D3D.GetContext()->DSSetShader(mDS, NULL, 0);
+	//}
+
 }
 
 // private
 void Shader::InitializeShader(IS_Type type)
 {
 	// 컴파일
-	wstring path = L"GameSource\\" + mShaderName + L".fx";
+	wstring path = L"GameSource\\" + mName + L".fx";
 
-	char* entry;
-	char* profile;
+	char* entry = "";
+	char* profile = "";
 
 	switch (type)
 	{
@@ -76,16 +92,16 @@ void Shader::InitializeShader(IS_Type type)
 	{
 		// **error 해제는 WriteShaderLog함수에서 함
 		WriteShaderLog(error);
-		return;
+		assert(0);
 	}
 
 	// 각 쉐이더 버퍼생성
 	switch (type)
 	{
-	case IS_Vertex: hr = D3D.GetDevice()->CreateVertexShader(mShaderBlob->GetBufferPointer(), mShaderBlob->GetBufferSize(), NULL, &mVertexShader);
-	case IS_Hull:	hr = D3D.GetDevice()->CreateHullShader(mShaderBlob->GetBufferPointer(), mShaderBlob->GetBufferSize(), NULL, &mHullShader);
-	case IS_Domain: hr = D3D.GetDevice()->CreateDomainShader(mShaderBlob->GetBufferPointer(), mShaderBlob->GetBufferSize(), NULL, &mDomainShader);
-	case IS_Pixel:	hr = D3D.GetDevice()->CreatePixelShader(mShaderBlob->GetBufferPointer(), mShaderBlob->GetBufferSize(), NULL, &mPixelShader);
+	case IS_Vertex: hr = D3D.GetDevice()->CreateVertexShader(mShaderBlob->GetBufferPointer(), mShaderBlob->GetBufferSize(), NULL, &mVS); break;
+	case IS_Hull:	hr = D3D.GetDevice()->CreateHullShader(mShaderBlob->GetBufferPointer(), mShaderBlob->GetBufferSize(), NULL, &mHS); break;
+	case IS_Domain: hr = D3D.GetDevice()->CreateDomainShader(mShaderBlob->GetBufferPointer(), mShaderBlob->GetBufferSize(), NULL, &mDS); break;
+	case IS_Pixel:	hr = D3D.GetDevice()->CreatePixelShader(mShaderBlob->GetBufferPointer(), mShaderBlob->GetBufferSize(), NULL, &mPS); break;
 	}
 }
 
--- RenderResource/Shader.h
+++ RenderResource/Shader.h
@@ -8,6 +8,7 @@ public:
 	Shader();
 	~Shader();
 	void Initialize(wstring shaderName, const D3D11_INPUT_ELEMENT_DESC* elementDesc, UINT elementCount, bool tessellated = false);
+	void SetShader();
 
 private:
 	typedef enum INITIALIZE_SHADER_ENUM
@@ -18,16 +19,16 @@ private:
 		IS_Pixel,
 		INITIALIZE_SHADER_COUNT,
 	} IS_Type;
-
-	wstring				mShaderName;
-	ID3D11VertexShader* mVertexShader;
-	ID3D11HullShader*	mHullShader;
-	ID3D11DomainShader* mDomainShader;
-	ID3D11PixelShader*	mPixelShader;
-	ID3D11InputLayout*	mInputLayout;
+	
+	wstring				mName;
+	bool				mTessellated;
+	ID3D11VertexShader* mVS;
+	ID3D11HullShader*	mHS;
+	ID3D11DomainShader* mDS;
+	ID3D11PixelShader*	mPS;
 	ID3D10Blob*			mShaderBlob;
+	ID3D11InputLayout*	mInputLayout;
 
 	void InitializeShader(IS_Type type);
 	void InitializeInputLayout(const D3D11_INPUT_ELEMENT_DESC* elementDesc, UINT elementCount);
-
 };
