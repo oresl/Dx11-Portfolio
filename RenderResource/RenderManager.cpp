ca7fe0f8e77341f03e0ddbe09e8bc5c21d3e6528
orseL <freundliche.alien@gmail.com>
2018-04-04 00:29:33 +0900

    Render Manager

--- RenderResource/RenderManager.cpp
+++ RenderResource/RenderManager.cpp
@@ -7,6 +7,8 @@ Singleton_cpp(RenderManager)
 RenderManager::RenderManager()
 {
 	mShaderList.clear();
+	mTextureList.clear();
+	mTextureArrayList.clear();
 }
 
 RenderManager::~RenderManager()
@@ -16,6 +18,30 @@ RenderManager::~RenderManager()
 		SafeDeletePtr(shader.second);
 	}
 	mShaderList.clear();
+
+	for (auto texture : mTextureList)
+	{
+		SafeReleaseCom(texture.second);
+	}
+	mTextureList.clear();
+
+	for (auto textureArray : mTextureArrayList)
+	{
+		SafeReleaseCom(textureArray.second);
+	}
+	mTextureArrayList.clear();
+}
+
+void RenderManager::Initialize()
+{
+	mRasterizerState = new RasterizerState;
+	mRasterizerState->Initialize();
+
+	mBlendState = new BlendState;
+	mBlendState->Initialize();
+
+	mDepthStencilState = new DepthStencilState;
+	mDepthStencilState->Initialize();
 }
 
 void RenderManager::AddShader(wstring shaderName, const D3D11_INPUT_ELEMENT_DESC* elementDesc, UINT elementCount, bool tessellated)
@@ -31,14 +57,108 @@ void RenderManager::AddShader(wstring shaderName, const D3D11_INPUT_ELEMENT_DESC
 	mShaderList.insert(make_pair(shaderName, newShader));
 }
 
+void RenderManager::AddTexture(wstring textureName, EXT_Type ext)
+{
+	auto texture = mTextureList.find(textureName);
+	if (texture != mTextureList.end())
+		return;
+
+	wstring extensionName;
+	switch (ext)
+	{
+	case TE_DDS: extensionName = L"dds"; break;
+	case TE_PNG: extensionName = L"png"; break;
+	case TE_JPG: extensionName = L"jpg"; break;
+	case TE_GIF: extensionName = L"gif"; break;
+	case TE_BMP: extensionName = L"bmp"; break;
+	}
+
+	ID3D11ShaderResourceView* newTexture;
+	CreateShaderResourceView(D3D.GetDevice(), textureName, extensionName, &newTexture);
+
+	mTextureList.insert(make_pair(textureName, newTexture));
+}
+
+void RenderManager::AddTextureArray(wstring arrayName, vector<wstring> textureNames, EXT_Type ext)
+{
+	auto textureArray = mTextureArrayList.find(arrayName);
+	if (textureArray != mTextureArrayList.end())
+		return;
+
+	wstring extensionName;
+	switch (ext)
+	{
+	case TE_DDS: extensionName = L"dds"; break;
+	case TE_PNG: extensionName = L"png"; break;
+	case TE_JPG: extensionName = L"jpg"; break;
+	case TE_GIF: extensionName = L"gif"; break;
+	case TE_BMP: extensionName = L"bmp"; break;
+	}
+
+	ID3D11ShaderResourceView* newTexture;
+	CreateShaderResourceViewArray(D3D.GetDevice(), D3D.GetContext(), textureNames, extensionName, &newTexture);
+
+	mTextureList.insert(make_pair(arrayName, newTexture));
+}
+
 void RenderManager::SetShader(wstring shaderName)
 {
 	auto shader = mShaderList.find(shaderName);
 
 	if (shader == mShaderList.end())
+	{
+		WriteLog(L"No shader in List", L_Warning);
 		return;
+	}
 
 	shader->second->SetShader();
+}
+
+void RenderManager::SetTexture(wstring textureName, UINT startSlot)
+{
+	auto texture = mTextureList.find(textureName);
+
+	if (texture == mTextureList.end())
+	{
+		WriteLog(L"No texture in List", L_Warning);
+		return;
+	}
+
+	D3D.GetContext()->VSSetShaderResources(startSlot, 1, &texture->second);
+	D3D.GetContext()->HSSetShaderResources(startSlot, 1, &texture->second);
+	D3D.GetContext()->DSSetShaderResources(startSlot, 1, &texture->second);
+	D3D.GetContext()->PSSetShaderResources(startSlot, 1, &texture->second);
+}
+
+void RenderManager::SetRasterizerState(RS_Type type)
+{
+	mRasterizerState->SetState(type);
+}
+
+void RenderManager::SetTextureArray(wstring arrayName, UINT startSlot)
+{
+	auto textureArray = mTextureArrayList.find(arrayName);
+
+	if (textureArray == mTextureArrayList.end())
+	{
+		WriteLog(L"No texture array in List", L_Warning);
+		return;
+	}
+
+	D3D.GetContext()->VSSetShaderResources(startSlot, 1, &textureArray->second);
+	D3D.GetContext()->HSSetShaderResources(startSlot, 1, &textureArray->second);
+	D3D.GetContext()->DSSetShaderResources(startSlot, 1, &textureArray->second);
+	D3D.GetContext()->PSSetShaderResources(startSlot, 1, &textureArray->second);
+}
+
+void RenderManager::SetBlendState(BS_Type type)
+{
+	mBlendState->SetState(type);
+}
+
+void RenderManager::SetDepthStencilState(DS_Type type)
+{
+	mDepthStencilState->SetState(type);
 }
 
 void RenderManager::SetIAParameter(InputLayout* object)
--- RenderResource/RenderManager.h
+++ RenderResource/RenderManager.h
@@ -1,22 +1,48 @@
 #pragma once
 #include "..\Common\Pch.h"
 #include "..\Graphic\Direct3D.h"
+#include "..\Graphic\RasterizerState.h"
+#include "..\Graphic\BlendState.h"
+#include "..\Graphic\DepthStencilState.h"
 #include "Shader.h"
 #include "InputLayout.h"
+
+typedef enum TEXTURE_EXTENSION_ENUM
+{
+	TE_DDS = 0,
+	TE_PNG,
+	TE_JPG,
+	TE_GIF,
+	TE_BMP,
+	TEXTURE_EXTENSION_COUNT,
+} EXT_Type;
 
 class RenderManager
 {
 public:
 	RenderManager();
 	~RenderManager();
+	void Initialize();
 	void AddShader(wstring shaderName, const D3D11_INPUT_ELEMENT_DESC* elementDesc, UINT elementCount, bool tessellated = false);
+	void AddTexture(wstring textureName, EXT_Type ext);
+	void AddTextureArray(wstring arrayName, vector<wstring> textureNames, EXT_Type ext);
 	void SetShader(wstring shaderName);
+	void SetTexture(wstring textureName, UINT startSlot);
+	void SetTextureArray(wstring arrayName, UINT startSlot);
+	void SetRasterizerState(RS_Type type);
+	void SetBlendState(BS_Type type);
+	void SetDepthStencilState(DS_Type type);
 	void SetIAParameter(InputLayout* object);
 
 	Singleton_h(RenderManager)
 
 private:
-	map<wstring, Shader*> mShaderList;
+	map<wstring, Shader*>					mShaderList;
+	map<wstring, ID3D11ShaderResourceView*> mTextureList;
+	map<wstring, ID3D11ShaderResourceView*> mTextureArrayList;
+	RasterizerState*						mRasterizerState;
+	BlendState*								mBlendState;
+	DepthStencilState*						mDepthStencilState;
 
 };
 
