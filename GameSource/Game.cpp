1b04c328d1a2a8604677db6a6c9276af2b6aea91
orseL <freundliche.alien@gmail.com>
2018-04-04 05:43:38 +0900

    no summary

--- GameSource/Game.cpp
+++ GameSource/Game.cpp
@@ -16,21 +16,19 @@ Game::~Game()
 
 void Game::Initialize()
 {
-	mCamera = new Camera;
-	mCamera->Initialize();
-	mCamera->SetPosition(0.0F, 0.0F, -10.0F);
-
-	InitializeRenderManager();
+	InitializeRenderResource();
 	InitializeGameObject();
 }
 
 void Game::Update(FLOAT delta)
 {
-	mCamera->Update(delta);
-	mCamera->UpdateReflection(0.0F);
-	mCamera->SetBuffer();
+	UpdateRenderResource(delta);
+	UpdateGameObject(delta);
 
 	if (Input.KeyUp(DIK_1))
+		mWireFrame = !mWireFrame;
+
+	if (Input.KeyUp(DIK_2))
 		mWireFrame = !mWireFrame;
 
 	if (Input.KeyUp(DIK_ESCAPE))
@@ -42,6 +40,29 @@ void Game::PreRender()
 
 void Game::Render()
 {
+	D3DXVECTOR3 camPos = mCamera->GetPosition();
+
+	// SkySphere
+	RenderMgr.SetRasterizerState(RS_CullNone);
+	RenderMgr.SetDepthStencilState(DS_DepthDisable);
+	RenderMgr.SetShader(L"SkySphere");
+	mSkySphere->SetPosition(camPos.x, camPos.y, camPos.z);
+	mSkySphere->SetBuffer();
+	RenderMgr.SetIAParameter(mSkySphere);
+
+	// SkyPlane
+	RenderMgr.SetRasterizerState(RS_CullBack);
+	RenderMgr.SetBlendState(BS_SkyPlane);
+	RenderMgr.SetShader(L"SkyPlane");
+	mSkyPlane->SetPosition(camPos.x, camPos.y, camPos.z);
+	mSkyPlane->SetBuffer();
+	RenderMgr.SetTexture(L"Cloud", 0);
+	RenderMgr.SetTexture(L"Noise", 1);
+	RenderMgr.SetIAParameter(mSkyPlane);
+	RenderMgr.SetBlendState(BS_Default);
+	RenderMgr.SetDepthStencilState(DS_Default);
+
+	// Cube0
 	RenderMgr.SetShader(L"Cube");
 	RenderMgr.SetRasterizerState(mWireFrame ? RS_WireFrame_CullBack : RS_CullBack);
 	mCube->SetBuffer();
@@ -54,14 +75,87 @@ void Game::InitializeGameObject()
 {
 	mCube = new Cube;
 	mCube->Initialize();
+	
+	mSkySphere = new SkySphere;
+	mSkySphere->Initialize();
+
+	mSkyPlane = new SkyPlane;
+	mSkyPlane->Initialize();
 }
 
-void Game::InitializeRenderManager()
+void Game::InitializeRenderResource()
 {
+	// Camera, Frustum
+	mCamera = new Camera;
+	mCamera->Initialize();
+	mCamera->SetPosition(0.0F, 0.0F, -10.0F);
+
+	mFrustum = new Frustum;
+
+	// Light, Material
+	mLight = new Light;
+	mLight->Initialize();
+
+	D3DXVECTOR4 ambient;
+	D3DXVECTOR4 diffuse;
+	D3DXVECTOR4 specular;
+	FLOAT		specularPower;
+
+	// Specular x
+	ambient			= D3DXVECTOR4(0.15F, 0.15F, 0.15F, 1.0F);
+	diffuse			= D3DXVECTOR4(1.0F, 1.0F, 1.0F, 1.0F);
+	specular		= D3DXVECTOR4(0.0F, 0.0F, 0.0F, 1.0F);
+	specularPower	= 0.0F;
+
+	mMaterial0 = new Material;
+	mMaterial0->Initialize();
+	mMaterial0->SetMaterial(ambient, diffuse, specular, specularPower);
+
+	// Specular o
+	ambient			= D3DXVECTOR4(0.15F, 0.15F, 0.15F, 1.0F);
+	diffuse			= D3DXVECTOR4(1.0F, 1.0F, 1.0F, 1.0F);
+	specular		= D3DXVECTOR4(1.0F, 1.0F, 1.0F, 1.0F);
+	specularPower	= 32.0F;
+
+	mMaterial1 = new Material;
+	mMaterial1->Initialize();
+	mMaterial1->SetMaterial(ambient, diffuse, specular, specularPower);
+
+	// Shader, Texture
 	RenderMgr.Initialize();
 
 	RenderMgr.AddShader(L"Cube", IL_PosTexNor::sElementDesc, IL_PosTexNor::sElementCount, false);
+	RenderMgr.AddShader(L"SkySphere", IL_Pos::sElementDesc, IL_Pos::sElementCount, false);
+	RenderMgr.AddShader(L"SkyPlane", IL_PosTex::sElementDesc, IL_PosTex::sElementCount, false);
 
 	RenderMgr.AddTexture(L"Test", TE_GIF);
 	RenderMgr.AddTexture(L"Trapezoid", TE_JPG);
+	RenderMgr.AddTexture(L"Cloud", TE_DDS);
+	RenderMgr.AddTexture(L"Noise", TE_DDS);
+}
+
+void Game::UpdateGameObject(FLOAT delta)
+{
+	mSkyPlane->Update(delta);
+}
+
+void Game::UpdateRenderResource(FLOAT delta)
+{
+	// Camera, Frustum
+	mCamera->Update(delta);
+	mCamera->UpdateReflection(0.0F);
+	mCamera->SetBuffer();
+
+	D3DXMATRIX view			= mCamera->GetView();
+	D3DXMATRIX projection	= mCamera->GetProjection();
+	D3DXMATRIX viewProj;
+	D3DXMatrixMultiply(&viewProj, &view, &projection);
+	mFrustum->SetFrustum(viewProj);
+
+	// Light, Material
+	mLight->Update(delta);
+	mLight->SetBuffer();
+
+	mMaterial0->SetBuffer(RB_Material0);
+	mMaterial1->SetBuffer(RB_Material1);
 }
--- GameSource/Game.h
+++ GameSource/Game.h
@@ -1,26 +1,42 @@
 #pragma once
 #include "..\Common\Pch.h"
-#include "..\Graphic\Camera.h"
+#include "..\RenderResource\Camera.h"
+#include "..\RenderResource\Frustum.h"
+#include "..\RenderResource\Light.h"
+#include "..\RenderResource\Material.h"
 #include "..\RenderResource\RenderManager.h"
+
 #include "Cube.h"
+#include "SkySphere.h"
+#include "SkyPlane.h"
 
 class Game
 {
 public:
 	Game();
 	~Game();
-	void Initialize();
-	void Update(FLOAT delta);
-	void PreRender();
-	void Render();
+	void		Initialize();
+	void		Update(FLOAT delta);
+	void		PreRender();
+	void		Render();
 
 private:
-	bool mWireFrame;
+	bool		mWireFrame;
+	
+	Camera*		mCamera;
+	Frustum*	mFrustum;
+	Light*		mLight;
+	Material*	mMaterial0;
+	Material*	mMaterial1;
 
-	Camera* mCamera;
-	Cube* mCube;
+	Cube*		mCube;
+	SkySphere*	mSkySphere;
+	SkyPlane*	mSkyPlane;
 
-	void InitializeRenderManager();
-	void InitializeGameObject();
+	void		InitializeRenderResource();
+	void		InitializeGameObject();
+	void		UpdateRenderResource(FLOAT delta);
+	void		UpdateGameObject(FLOAT delta);
+
 };
 
